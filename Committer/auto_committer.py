e9b852f46cb32902b5abe54fbb67bdd1dce48881
Jesus <chuy8jay@gmail.com>
2017-07-16 18:40:05 -0600

    Able to get commits of certain repo name

--- Committer/auto_committer.py
+++ Committer/auto_committer.py
@@ -6,5 +6,21 @@ g = Github("zarate8", Password.getPassword())
 
 repos = g.get_user().get_repos()
 
+repo_name = 'bkimmig/data-mining-project'
+repo_name = 'zarate8/AutomaticGitCommitter'
+
+rep = g.get_repo(repo_name)
+
+#coms = rep.get_commits()
+
+# Get commits of certain repo name
+for commit in g.get_repo(repo_name).get_commits():
+    print (commit.commit.raw_data["committer"]["date"])
+
+'''
+Get the commits of the first repo
+'''
+'''
 for commit in repos[0].get_commits():
     print (commit.commit.raw_data["committer"]["date"])
+'''
