50746884bab2bf644a9b022a18f160cbcc8e0968
Jesus <chuy8jay@gmail.com>
2017-07-16 18:54:59 -0600

    Created functianlity if functions and documented

--- Committer/auto_committer.py
+++ Committer/auto_committer.py
@@ -4,23 +4,37 @@ import Password
 # First create a Github instance:
 g = Github("zarate8", Password.getPassword())
 
-repos = g.get_user().get_repos()
-
 repo_name = 'bkimmig/data-mining-project'
 repo_name = 'zarate8/AutomaticGitCommitter'
 
-rep = g.get_repo(repo_name)
 
-#coms = rep.get_commits()
+def getDatesOfCommitsOfRepo(repo_name):
+    """
+    Get commits of certain repo name
+    dates of each commit
+    :param repo_name:
+    :return: dates of the commits of the specified repo name
+    """
+    dates = []
+    for commit in g.get_repo(repo_name).get_commits():
+        dates.append(commit.commit.raw_data["committer"]["date"])
 
-# Get commits of certain repo name
-for commit in g.get_repo(repo_name).get_commits():
-    print (commit.commit.raw_data["committer"]["date"])
+    return dates
 
-'''
-Get the commits of the first repo
-'''
-'''
-for commit in repos[0].get_commits():
-    print (commit.commit.raw_data["committer"]["date"])
-'''
+
+def getDatesOfCommits():
+    """
+    Get the commits of the first repo
+
+    :return: dates of the commits
+    """
+
+    dates = []
+    repos = g.get_user().get_repos()
+    for i in range(0, len(repos)):
+        for commit in repos[i].get_commits():
+            dates.append(commit.commit.raw_data["committer"]["date"])
+    return dates
+
+for date in getDatesOfCommitsOfRepo(repo_name):
+    print date
